5dd3acd37d1ab6f759d9893531bf7413eb1a5845
Marcus Rosette <rosettem@oregonstate.edu>
2023-07-30 16:26:45 -0700

    adding functions to map lin pot readings

--- scripts/process_data.py
+++ scripts/process_data.py
@@ -11,6 +11,9 @@ class ProcessData():
         self.joint_positions = None
         self.voltage_reading = None
 
+        self.mapped_reading = None
+        self.pitch = None
+
     def parse_data_file(self, filename, remove_header=True, num_initial_line_skip=0):
         with open(filename,'r') as file:
             for i, line in enumerate(file):
@@ -43,33 +46,48 @@ class ProcessData():
         if clamp_joint_4:
             self.joint_positions[:, 3] = 0
 
+    def voltage_map(self, des_low, des_high, v_low=0, v_high=5):
+        """Maps voltage readings to a specified range"""
+        self.mapped_reading = np.interp(self.voltage_reading, (v_low, v_high), (des_low, des_high))
+    
+    def pitch_extrapolation(self, spring_mount_height=0.6096, rad2deg=False):
+        """Converting the linear potentiometer readings to pitch angle"""
+        # Using arc length
+        self.pitch = self.voltage_reading / spring_mount_height
+
+        if rad2deg:
+            self.pitch = np.degrees(self.pitch)
+
     def plot_data(self, x, y, title='', ylabel='', xlabel='', legend=[]):
         plt.plot(x, y)
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
         plt.title(title)
         plt.legend(legend)
-
         plt.show()
 
 
 if __name__ == '__main__':    
     pdata = ProcessData()
 
+    # Parse log file nad extract variable elements
     filename = "logs/2023-07-29-18-13-04.log"
     pdata.parse_data_file(filename, num_initial_line_skip=10)
     pdata.extract_elements()
 
-    pdata.plot_data(pdata.timestamp, pdata.voltage_reading, 
-                    title='Voltage over Bravo Trajectory', 
-                    ylabel='Voltage (V)', 
-                    xlabel='Time (s)')
-    
+    # Map voltage value to amount pitched
+    pdata.voltage_map(0, 0.5)
+    pdata.pitch_extrapolation(rad2deg=True)    
+
+    # Plot data
     pdata.plot_data(pdata.timestamp, pdata.joint_positions, 
                     title='Bravo Trajectory', 
                     ylabel='Position (rad)', 
                     xlabel='Time (s)',
                     legend=['joint 1', 'joint 2', 'joint 3', 'joint 4', 'joint 5', 'joint 6', 'joint 7'])
 
+    pdata.plot_data(pdata.timestamp, pdata.pitch, 
+                    title='Frame Pitch over Bravo Trajectory', 
+                    ylabel='Pitch (deg)', 
+                    xlabel='Time (s)')
     
-
